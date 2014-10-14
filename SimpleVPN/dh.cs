8fc8cd391494bb18d81714be8a59b1087df48cf4
Sandy Buchanan <s.buchanan@alumni.ubc.ca>
2014-10-14 00:00:25 -0700

    Diffie Hellman exchange. Warning that there is a known overflow bug that I'm still working on.

---
+++ SimpleVPN/dh.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SimpleVPN
+{
+    public class dh
+    {
+        public int p { get; set; }		// Prime number
+        public int g { get; set; }		// Base g
+		private int a { get; set; }     // Generated secret number
+        public double key { get; set; } // Shared secret key
+        
+        public dh(int p, int g)
+        {
+			this.p = p;
+			this.g = g;
+        }
+
+		public double generateClientKey()
+		{
+			// Generate secret number a
+			this.a = 6; //Random number smaller than p
+												
+			return Math.Pow(this.g , this.a) % this.p;
+		}
+
+		public void calculateSessionKey(double b)
+		{
+			// Calculate secret key from a and b
+			this.key = Math.Pow(this.p, b) % this.p;
+												
+			// Forget everything except the key
+			this.a = 0;
+			this.g = 0;
+			this.p = 0;
+		}
+    }
+}
